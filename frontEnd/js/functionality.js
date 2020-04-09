f75b0e6a403004b9e7fde9bc5e2bd828a2e01331
Anuj Parakh <anuj@anujinfotech.com>
2020-04-09 14:07:57 -0500

    added settings sidebar with profile picture and dark mode, color blindness mode options.

--- frontEnd/js/functionality.js
+++ frontEnd/js/functionality.js
@@ -1,6 +1,77 @@
 
+var transEndEventName = ('WebkitTransition' in document.documentElement.style) ? 'webkitTransitionEnd' : 'transitionend';
 
 function oldTripsClicked()
 {
+    var sidebar = document.getElementById("sidebar");
+    // If sidebar is open, collapse it
+    if (!sidebar.classList.contains("collapsed"))
+    {
+        toggleSidebar();
+        if (document.getElementById("old-trips-container").style.display == "flex")
+        return;
+
+        document.addEventListener(transEndEventName , showOldTrips);
+        return;
+    }
+    
+    showOldTrips();
+    
+    // TODO: Here we will get the information about the saved trips
+}
+
+function toggleTheme()
+{
+    // Toggle all light themed classes
+
+    // Toggle all dark themed classes
+}
+
+function settingsClicked()
+{
+    var sidebar = document.getElementById("sidebar");
+    // If sidebar is open, collapse it
+    if (!sidebar.classList.contains("collapsed"))
+    {
+        toggleSidebar();
+
+        if (document.getElementById("settings-container").style.display == "flex")
+        return;
+        
+        document.addEventListener(transEndEventName , showSettings);
+        return;
+    }
+    
+    showSettings();
+
+    // TODO: Here we will get the information about the person/settings
+    
+}
+
+function showSettings()
+{
+    // Hide Old Trips and show Settings
+    document.getElementById("settings-container").style.display = 'flex';
+    document.getElementById("old-trips-container").style.display = 'none';
+
+    // Open the sidebar again
+    toggleSidebar();
+    document.removeEventListener(transEndEventName, showSettings);
+}
+
+function showOldTrips()
+{
+    // Show Old Trips and hide Settings
+    document.getElementById("settings-container").style.display = 'none';
+    document.getElementById("old-trips-container").style.display = 'flex';
+
+    // Open the sidebar again
+    toggleSidebar();
+    document.removeEventListener(transEndEventName, showOldTrips);
+}
+
+function toggleSidebar()
+{
     document.getElementById("sidebar").classList.toggle("collapsed");
 }
+
