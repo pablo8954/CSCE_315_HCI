36d9525ffcb8c5d520bdf87d01246c2251d558b6
Anuj Parakh <anuj@anujinfotech.com>
2020-04-13 15:28:23 -0500

    added dark mode functionality, fixed minor bugs

--- frontEnd/js/functionality.js
+++ frontEnd/js/functionality.js
@@ -1,5 +1,27 @@
 
+// Variables to keep track of
 var transEndEventName = ('WebkitTransition' in document.documentElement.style) ? 'webkitTransitionEnd' : 'transitionend';
+var darkMode = sessionStorage.getItem('dark-mode');
+
+console.log('dark mode found as ' + darkMode)
+
+if (darkMode)
+{
+    // Set as opposite of what it is
+    darkMode = (darkMode == "true");
+    if (darkMode)
+    {
+        darkMode = false;
+        document.getElementById('dark-mode-checkbox').checked = true;
+        toggleTheme();
+    }
+}
+else
+{
+    darkMode = false;
+}
+
+var colorBlindness = false;
 
 function oldTripsClicked()
 {
@@ -22,9 +44,33 @@ function oldTripsClicked()
 
 function toggleTheme()
 {
-    // Toggle all light themed classes
+    darkMode = !darkMode;
+    sessionStorage.setItem('dark-mode', JSON.stringify(darkMode));
+    var toCheck = sessionStorage.getItem('dark-mode');
+    console.log ('set to ' + darkMode);
+    console.log(typeof darkMode);
+    // ** To change **
+    // main-body
+    // logo heading
+    // main-nav
+    // big-label
+    // small-label
 
     // Toggle all dark themed classes
+    toggleDarkThemeOfClass("main-body", "dark");
+    toggleDarkThemeOfClass("main-nav-link", "dark-main-nav");
+    toggleDarkThemeOfClass("nav-logo-label", "dark-nav-logo-label");
+    toggleDarkThemeOfClass("small-label", "dark-small-label");
+    toggleDarkThemeOfClass("big-label", "dark-big-label");
+}
+
+function toggleDarkThemeOfClass(className, darkClassName)
+{
+    var toChange = document.getElementsByClassName(className);
+    for (var i = 0; i < toChange.length; ++i)
+    {
+        toChange[i].classList.toggle(darkClassName);
+    }
 }
 
 function settingsClicked()
